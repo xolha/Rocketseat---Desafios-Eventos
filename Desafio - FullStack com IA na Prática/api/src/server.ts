ac2fda96de68e7e7edef8adaaf492e0b6bd2bcdd
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-10-27 23:19:05 -0300

    ✨ feat: config initial backend

---
+++ Desafio - FullStack com IA na Prática/api/src/server.ts
@@ -0,0 +1,42 @@
+import fastify from 'fastify'
+import {
+    serializerCompiler,
+    validatorCompiler,
+    jsonSchemaTransform,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifyCors} from "@fastify/cors";
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+
+
+const App = fastify().withTypeProvider<ZodTypeProvider>()
+
+App.setValidatorCompiler(validatorCompiler)
+App.setSerializerCompiler(serializerCompiler)
+
+App.register(fastifyCors, {
+    origin: true,
+    methods: [ "GET","POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    // credential: true,
+})
+
+App.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "Webhook Inspector API",
+            version: "1.0.0",
+            description: "API for capturing and inspecting webhook requests",
+        },
+    },
+    transform: jsonSchemaTransform,
+})
+
+App.register(ScalarApiReference, {
+    routePrefix: "/docs", //quando o alguém olhar essa rota, ele irá olhar essa documentação (óbvio)
+})
+
+App.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+    console.log('🔥 HTTP server running on http://localhost:3333');
+    console.log('📚 Docs available on http://localhost:3333/docs');
+})
