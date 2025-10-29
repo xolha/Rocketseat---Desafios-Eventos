1c2a85a2825c3396ba0eca269f883c5ec2ab2ca2
Júlia Porto Alvarenga <juliaportojpa@gmail.com>
2025-10-29 15:39:13 -0300

    ✨ feat: add IconButton component

---
+++ Desafio - FullStack com IA na Prática/web/src/components/ui/icon-button.tsx
@@ -0,0 +1,31 @@
+import type {ComponentProps, ReactNode} from "react";
+import {tv, type VariantProps} from 'tailwind-variants';
+
+const iconButton = tv({
+    base: 'flex items-center justify-center rounded-lg hover:bg-zinc-700 transition-colors duration-160',
+    variants: {
+        size: {
+            sm: 'size-6',
+            md: 'size-8',
+        },
+    },
+    defaultVariants: {
+        size: 'md',
+    },
+})
+
+interface IconButtonProps extends ComponentProps<'button'>, VariantProps<typeof IconButton> {
+    icon: ReactNode,
+}
+
+export function IconButton({ icon, size, className, ...props}: IconButtonProps) {
+    return (
+        <button
+            type="button"
+            className={iconButton({size})}
+            {...props}
+        >
+            {icon}
+        </button>
+    )
+}
